727666f9eb05e69ba0eb728b480ae49b9cb1bc72
SamMcRuvie <doctorwhat35@gmail.com>
2018-04-18 23:23:18 +0100

    RiverState: fix succsessormethod

--- src/RiverCrossRun.java
+++ src/RiverCrossRun.java
@@ -18,18 +18,14 @@ public class RiverCrossRun {
         System.out.println(RiverCrossProblem.RAFT_MAX_WEIGHT);
 
         southBank = new HashSet<Person>();
-        southBank.add(new Person("Adam", 30, true));
+        southBank.add(new Person("Adam", 30, false));
         southBank.add(new Person("Betty", 100, false));
         southBank.add(new Person("Claire", 50, true));
         southBank.add(new Person("Dave", 40, false));
         southBank.add(new Person("John", 30, true));
         southBank.add(new Person("Max", 100, false));
         southBank.add(new Person("Sam", 50, false));
-        southBank.add(new Person("Tom", 40, true));
-        southBank.add(new Person("Adam", 30, true));
-        southBank.add(new Person("Betty", 100, false));
-        southBank.add(new Person("Claire", 50, true));
-        southBank.add(new Person("Dave", 40, true));
+
 
         northBank = new HashSet<>();
         RiverCrossState initialState = new RiverCrossState(northBank, southBank, RiverBank.SOUTH);
